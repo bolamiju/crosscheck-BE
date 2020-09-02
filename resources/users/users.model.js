1fed5d593e0fca9209989dea933397e2cc89d4c7
tolaked <adetolaakere1@gmail.com>
2020-09-02 10:01:16 +0100

    user model

---
+++ resources/users/users.model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const UserSchema = Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+    unique: true,
+  },
+  organizationName:{
+    type:String,
+    required:false,
+    minlength:2,
+    maxlength:50,
+    unique:true
+  },
+  country:{
+    type:String,
+    required:true,
+    minlength:2,
+    maxlength:50
+  },
+  phone:{
+    type:String,
+    required:true,
+    minlength:7,
+    maxlength:50,
+    unique:true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 50,
+  },
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
