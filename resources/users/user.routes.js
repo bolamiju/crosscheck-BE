36b9646ce91248768a23ff10690a05d39ab04546
tolaked <adetolaakere1@gmail.com>
2020-09-04 11:09:08 +0100

    login endpoint

--- resources/users/user.routes.js
+++ resources/users/user.routes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 const userController = require("./userController");
 
-const { register } = userController;
+const { register,login } = userController;
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
