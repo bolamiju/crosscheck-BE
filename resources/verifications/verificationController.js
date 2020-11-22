13dfd3c5f8a6134e58bc53ed0168c60b622e7149
tolaked <adetolaakere1@gmail.com>
2020-11-22 11:34:37 +0100

    verification paramaters

--- resources/verifications/verificationController.js
+++ resources/verifications/verificationController.js
@@ -19,6 +19,7 @@ const requestVerification = async (req, res) => {
       amount,
       email,
     } = req.body;
+
     const today = new Date();
     const day = String(today.getDate()).padStart(2, "0");
     const month = String(today.getMonth() + 1).padStart(2, "0");
