38e98a9ce9dc068922fac1f67176e658e1a3e52f
tolaked <adetolaakere1@gmail.com>
2020-12-19 19:55:28 +0100

    fix uploads issue

--- resources/verifications/verification.routes.js
+++ resources/verifications/verification.routes.js
@@ -6,7 +6,7 @@ const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // cb(null, "./uploads/");
-    cb(null, path.resolve(__dirname, "./uploads/"));
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: function (req, file, cb) {
     cb(
